d8e13f67f23658fafcda72ca28bc2eecf66b24bf
DerAngell <ofitserovd@gmail.com>
2020-07-14 13:02:04 +0300

    qutebrowser

---
+++ qutebrowser/config.py
@@ -0,0 +1,14 @@
+c.auto_save.session = True
+c.content.autoplay = False
+c.content.headers.accept_language = "ru-RU,ru;q=0.9"
+c.content.notifications = False
+c.downloads.position = "bottom"
+c.scrolling.bar = "always"
+c.scrolling.smooth = True
+c.session.lazy_restore = True
+c.spellcheck.languages = ['en-US', 'ru-RU']
+c.tabs.background = True
+c.url.searchengines["!y"] = "https://yandex.by/search/?lr=157&text={}"
+c.url.searchengines["!g"] = "https://www.google.com/search?q={}"
+c.url.searchengines["!w"] = "https://ru.wikipedia.org/wiki/{}"
+c.url.searchengines["!yt"] = "https://www.youtube.com/results?search_query={}"
